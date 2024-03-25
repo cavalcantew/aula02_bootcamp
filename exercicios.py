c52dfe37fcb003ba6fd802e00f95b160a6276b1d
Wagner Cavalcante <wagnerc@gmail.com>
2024-03-25 14:49:43 -0300

    update projeto

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,36 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# numero_1 = int(input("Numero 1: "))
+# numero_2 = int(input("Numero 2: "))
+# soma = numero_1 + numero_2
+# print(soma)
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# numero_usuario = int(input("Digite um numero: "))
+# div_resto = numero_usuario % 5
+# print(f"O resto da divisao de {numero_usuario} por 5 é {div_resto}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# num_1 = int(input("Digite o primeiro numero: "))
+# num_2 = int(input("Digite o segundo numero: "))
+# result = num_1 * num_2
+# print("O resultado da multiplicação é:", result)
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# num_1 = int(input("Digite o primeiro numero: "))
+# num_2 = int(input("Digite o segundo numero: "))
+# try:
+#     result = num_1 // num_2
+#     print("O resultado da divisão inteira é:", result)
+# except ZeroDivisionError:
+#     print("Erro: divisão por zero não é permitida.")
+#     exit()
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+num = int(input("Digite um numero: "))
+resultado_quadrado = num ** 2
+print(f"O quadrado de {num} é:, {resultado_quadrado}")
 
 # #### Números de Ponto Flutuante (`float`)
 
